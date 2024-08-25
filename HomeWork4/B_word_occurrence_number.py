2e74b42aaea78dc8ccfc089d02933959723aa9c7
Mikhail Martin <JohnDoe110@yandex.ru>
2024-08-25 15:42:20 +0300

    сдана задача #B из домашки #4

--- HomeWork4/B_word_occurrence_number.py
+++ HomeWork4/B_word_occurrence_number.py
@@ -36,3 +36,15 @@ output: 0 0 0 0 0 0 1 0 0 1 0 0 1 0 2 2 0 0 0 0 1 2 3 3 1 1 4 0 1 0 1 2 4 1 5 0 
 input: aba aba; aba @?"
 output: 0 0 1 0
 """
+INPUT_FILE_NAME = "input.txt"
+
+
+if __name__ == "__main__":
+
+    input_file = open(INPUT_FILE_NAME, "r", encoding="utf8")
+
+    word_count = dict()
+    for line in input_file:
+        for word in line.split():
+            print(word_count.get(word, 0), end=" ")
+            word_count[word] = word_count.get(word, 0) + 1
