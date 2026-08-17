c78d21693e3238fb289cb28ad8886ddfbe7577f9
Mikhail Martin <JohnDoe110@yandex.ru>
2026-08-17 17:48:13 +0300

    сдана задача #C из домашки #6

--- HomeWork6/C_diplomas.py
+++ HomeWork6/C_diplomas.py
@@ -37,3 +37,83 @@
 input: 2 3 10
 output: 9
 """
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass
+class ProblemInput:
+    w: int
+    h: int
+    n: int
+
+
+class Solver:
+    def __init__(self, data: ProblemInput) -> None:
+        self.data = data
+
+    @property
+    def w(self) -> int:
+        return self.data.w
+
+    @property
+    def h(self) -> int:
+        return self.data.h
+
+    @property
+    def n(self) -> int:
+        return self.data.n
+
+    @classmethod
+    def from_stdin(cls) -> Self:
+        w, h, n = map(int, input().split())
+        return cls(ProblemInput(w, h, n))
+
+    @classmethod
+    def from_strings(cls, lines: list[str]) -> Self:
+        w, h, n = map(int, lines[0].split())
+        return cls(ProblemInput(w, h, n))
+
+    def solve(self) -> int:
+
+        min_width = self.w
+        max_width = self.w * self.n
+        min_height = self.h
+        max_height = self.h * self.n
+
+        min_size = max(min_width, min_height)
+        max_size = max(max_width, max_height)
+
+        size = self.left_binary_search(left=min_size, right=max_size)
+
+        return size
+
+    def left_binary_search(self, left: int, right: int) -> int:
+
+        while left < right:
+            middle = (left + right) // 2
+            if self.check(middle):
+                right = middle
+            else:
+                left = middle + 1
+
+        return left
+
+    def check(self, size: int) -> bool:
+
+        n_row = size // self.w
+        n_col = size // self.h
+
+        return self.n <= n_row * n_col
+
+
+def main() -> None:
+
+    solver = Solver.from_stdin()
+    result = solver.solve()
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/HomeWork6/test_C_diplomas.py
@@ -0,0 +1,22 @@
+import os
+import sys
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+import pytest
+from pytest import param
+from HomeWork6.C_diplomas import Solver
+
+
+@pytest.mark.parametrize(
+    ("lines", "expected"),
+    [
+        param(
+            ["2 3 10"],
+            9,
+        ),
+    ],
+)
+def test_solve(lines, expected):
+    solver = Solver.from_strings(lines)
+    assert solver.solve() == expected
